73e14c73c3398c4a4238c6488915471a14189364
doinel1a <doinel1atanasiu@gmail.com>
2024-01-03 19:03:32 +0100

    feat: implement github corner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 
+import GithubCorner from '@/components/github-corner';
+
 export default function Home() {
   return (
     <main className='flex h-full w-full flex-col items-center justify-center'>
+      <GithubCorner
+        title='Get started on GitHub'
+        url='https://github.com/doinel1a/next-ts-shadcn-ui'
+      />
       <h1>Hello, World!</h1>
     </main>
   );
