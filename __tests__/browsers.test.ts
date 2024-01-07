17583e1bd9e4839a542a0ff6e38a428aad676bd6
doinel1a <doinel1atanasiu@gmail.com>
2024-01-07 17:07:26 +0100

    refactor: test to check page title

--- __tests__/browsers.test.ts
+++ __tests__/browsers.test.ts
@@ -1,7 +1,9 @@
-import { test } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 
 import config from '../_config';
 
 test('Test browsers', async ({ page }) => {
   await page.goto(`http://${config.server.host}:${config.server.port}`);
+
+  await expect(page).toHaveTitle(config.metadata.title);
 });
