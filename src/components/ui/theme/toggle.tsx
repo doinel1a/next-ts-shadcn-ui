964e36c786e6f16c082c2c74fa17afcc21ad7566
doinel1a <doinel1atanasiu@gmail.com>
2024-01-07 17:31:25 +0100

    feat: implement theme toggle test

--- src/components/ui/theme/toggle.tsx
+++ src/components/ui/theme/toggle.tsx
@@ -19,22 +19,22 @@ export default function ThemeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant='outline' size='icon'>
+        <Button data-testid='theme-toggle' variant='outline' size='icon'>
           <Sun className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
           <MoonStar className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
           <span className='sr-only'>Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align='end'>
-        <DropdownMenuItem onClick={() => setTheme('light')}>
+      <DropdownMenuContent data-testid='theme-dropdown-content' align='end'>
+        <DropdownMenuItem data-testid='theme-light' onClick={() => setTheme('light')}>
           <Sun className='mr-2 h-[1.2rem] w-[1.2rem]' />
           Light
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('dark')}>
+        <DropdownMenuItem data-testid='theme-dark' onClick={() => setTheme('dark')}>
           <MoonStar className='mr-2 h-[1.2rem] w-[1.2rem]' />
           Dark
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('system')}>
+        <DropdownMenuItem data-testid='theme-system' onClick={() => setTheme('system')}>
           <Laptop className='mr-2 h-[1.2rem] w-[1.2rem]' />
           System
         </DropdownMenuItem>
