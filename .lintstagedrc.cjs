0e5c2e8ba52ff58ac444fc77010fe86a69a78aea
Doinel A. <92680721+doinel1a@users.noreply.github.com>
2024-12-15 19:34:14 +0100

    fix: lintstaged scripts
    Signed-off-by: Doinel A. <92680721+doinel1a@users.noreply.github.com>

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,9 +1,7 @@
 module.exports = {
-  'src/**/*.(ts|tsx)': () => 'npx tsc --noEmit',
-  'src/**/*.(css|scss|ts|tsx)': (filenames) => [`npx prettier --write ${filenames.join(' ')}`],
-  'src/**/*.(ts|tsx)': () => 'next lint'
-
-  // 'src/**/*.(ts|tsx)': (filenames) => [
-  //   `next lint --file ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --file ')}`
-  // ]
+  'src/**/*.(ts|tsx)': () => 'next lint',
+  'src/**/*.(ts|tsx)': () => 'tsc --noEmit',
+  'src/**/*.(html|css|scss|js|jsx|ts|tsx|mdx)': (filenames) => [
+    `prettier --write ${filenames.join(' ')} --cache`
+  ]
 };
