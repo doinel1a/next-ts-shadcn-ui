e22baed2017544a3a0c40312ea585946e1e66854
doinel1a <doinel1atanasiu@gmail.com>
2025-02-02 12:08:18 +0100

    chore: install & configure tanstack query

--- src/components/providers/root.tsx
+++ src/components/providers/root.tsx
@@ -2,10 +2,15 @@ import React from 'react';
 
 import type { PropsWithChildren } from 'react';
 
+import TanstackQueryProvider from './tanstack-query';
 import ThemeProvider from './theme';
 
 type TRootProvider = PropsWithChildren;
 
 export default function RootProvider({ children }: TRootProvider) {
-  return <ThemeProvider>{children}</ThemeProvider>;
+  return (
+    <ThemeProvider>
+      <TanstackQueryProvider>{children}</TanstackQueryProvider>
+    </ThemeProvider>
+  );
 }
