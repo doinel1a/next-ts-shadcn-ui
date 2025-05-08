5f8eb63ae0243025a05d68b49bb4f83cceb89120
doinel1a <doinel1atanasiu@gmail.com>
2025-05-08 20:56:15 +0200

    refactor: set project type as module

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,6 @@
-/** @type {import("prettier").Config} */
-module.exports = {
+/* eslint-disable import/no-anonymous-default-export */
+
+export default {
   printWidth: 100,
   tabWidth: 2,
   useTabs: false,
