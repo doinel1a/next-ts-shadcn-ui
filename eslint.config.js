36715e4fc1c3ca5b9e77954710b1f9bc65d518a4
doinel1a <doinel1atanasiu@gmail.com>
2025-05-08 20:59:21 +0200

    chore: upgrade & configure eslint to v9

---
+++ eslint.config.js
@@ -0,0 +1,61 @@
+import { FlatCompat } from '@eslint/eslintrc';
+import prettierConfig from 'eslint-config-prettier/flat';
+import prettierPlugin from 'eslint-plugin-prettier/recommended';
+import sonarjs from 'eslint-plugin-sonarjs';
+import unicorn from 'eslint-plugin-unicorn';
+import tseslint from 'typescript-eslint';
+
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname
+});
+
+export default tseslint.config(
+  {
+    ignores: ['.next']
+  },
+  /**
+   * Under the hood 'next' automatically configures:
+   * - eslint-plugin-next
+   * - eslint-plugin-react
+   * - eslint-plugin-react-hooks
+   * - eslint-plugin-jsx-a11y
+   */
+  ...compat.extends('next'),
+  prettierConfig,
+  prettierPlugin,
+  sonarjs.configs.recommended,
+  unicorn.configs.recommended,
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    extends: [
+      ...tseslint.configs.strict,
+      ...tseslint.configs.strictTypeChecked,
+      ...tseslint.configs.stylistic,
+      ...tseslint.configs.stylisticTypeChecked
+    ],
+    rules: {
+      '@typescript-eslint/array-type': 'off',
+      '@typescript-eslint/consistent-type-definitions': 'off',
+      '@typescript-eslint/consistent-type-imports': [
+        'warn',
+        { prefer: 'type-imports', fixStyle: 'inline-type-imports' }
+      ],
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/require-await': 'off',
+      '@typescript-eslint/no-misused-promises': [
+        'error',
+        { checksVoidReturn: { attributes: false } }
+      ]
+    }
+  },
+  {
+    linterOptions: {
+      reportUnusedDisableDirectives: true
+    },
+    languageOptions: {
+      parserOptions: {
+        projectService: true
+      }
+    }
+  }
+);
