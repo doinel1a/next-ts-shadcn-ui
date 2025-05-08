90758bfeed8154c396a86724dae3f728862c5e9f
doinel1a <doinel1atanasiu@gmail.com>
2025-05-08 20:56:58 +0200

    fix: lint staged config

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,20 @@
-module.exports = {
-  'src/**/*.(ts|tsx)': () => 'next lint',
-  'src/**/*.(ts|tsx)': () => 'tsc --noEmit',
-  'src/**/*.(html|css|scss|js|jsx|ts|tsx|mdx)': (filenames) => [
-    `prettier --write ${filenames.join(' ')} --cache`
-  ]
+// @ts-nocheck
+/* eslint-disable import/no-anonymous-default-export */
+
+import path from 'node:path';
+
+const tsc = () => 'tsc --noEmit';
+
+const eslint = (filenames) =>
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' --file ')}`;
+
+const prettier = (filenames) =>
+  `prettier --write ${filenames.map((f) => path.relative(process.cwd(), f)).join(' ')} --cache`;
+
+export default {
+  '*.{ts,tsx}': [tsc],
+  '*.{js,jsx,ts,tsx}': [eslint],
+  '*.{html,css,scss,js,jsx,cjs,mjs,ts,tsx,mdx}': [prettier]
 };
