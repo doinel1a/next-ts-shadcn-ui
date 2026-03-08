55ae908622480949a138d04190bda709b5e0c35c
doinel1a <doinel1atanasiu@gmail.com>
2026-03-08 19:26:11 +0100

    chore: update docs & metadata

--- _config.ts
+++ _config.ts
@@ -1,10 +1,10 @@
 const config = {
   metadata: {
-    title: 'Next.js 15 TypeScript & Shadcn/ui — Template',
+    title: 'Next.js 16 TypeScript & Shadcn/ui — Template',
     description:
-      'Boilerplate template designed to quickly bootstrap a Next.js Web App, SPA, website or landing page with Next.js 15, React.js 19, TypeScript, Shadcn/ui, TailwindCSS 4 and much more in just 30 seconds.',
+      'Boilerplate template designed to quickly bootstrap a Next.js Web App, SPA, website or landing page with Next.js 16, React.js 19, TypeScript, Shadcn/ui, TailwindCSS 4 and much more in just 30 seconds.',
     keywords:
-      'next.js 15, next.js-15-template, next.js-15-boilerplate, react.js 19, react.js-19-template, react.js-19-boilerplate, typescript, shadcn, shadcn/ui, tailwindcss 4, eslint, prettier, husky, commitlint, lint-staged'
+      'next.js 16, next.js-16-template, next.js-16-boilerplate, react.js 19, react.js-19-template, react.js-19-boilerplate, typescript, shadcn, shadcn/ui, tailwindcss 4, eslint, prettier, husky, commitlint, lint-staged'
   },
   server: {
     host: 'localhost',
