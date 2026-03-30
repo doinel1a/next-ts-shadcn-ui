b134f94b56b1de7a6db662fd3c0c9dec9f30a5ac
doinel1a <doinel1atanasiu@gmail.com>
2026-03-30 18:50:39 +0200

    fix: typescript errors

--- src/components/providers/client/theme.tsx
+++ src/components/providers/client/theme.tsx
@@ -4,14 +4,14 @@ import type { ThemeProviderProps } from 'next-themes';
 
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
 
-import EStorageKeys from '@/lib/constants/keys';
+import storageKeys from '@/lib/constants/keys';
 
 export default function ThemeProvider({ children, ...properties }: Readonly<ThemeProviderProps>) {
   return (
     <NextThemesProvider
       attribute='class'
       defaultTheme='system'
-      storageKey={EStorageKeys.theme}
+      storageKey={storageKeys.theme}
       disableTransitionOnChange
       enableSystem
       {...properties}
